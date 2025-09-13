3872d49109a4280debd35bcd98bada7ba2f93b99
Paul Quiñonez <polito98ec@gmail.com>
2025-09-13 13:07:57 -0500

    prepar mongo deb y envs

---
+++ src/app.module.ts
@@ -0,0 +1,37 @@
+import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+import { CommonModule } from './common/common.module';
+import { SeedModule } from './seed/seed.module';
+import { EnvConfiguration } from './config/env.config';
+import { JoiValidationSchema } from './config/joi.validation';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [EnvConfiguration],
+      validationSchema: JoiValidationSchema,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB'),
+        dbName: configService.get<string>('DBNAME'),
+      }),
+    }),
+    PokemonModule,
+    CommonModule,
+    SeedModule,
+  ],
+  controllers: [],
+  providers: [PokemonModule],
+})
+export class AppModule {}
